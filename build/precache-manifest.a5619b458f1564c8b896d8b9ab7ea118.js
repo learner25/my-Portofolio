3d0c3ba41c4a8f8146518960e22aaa4f7190889e
Saimon Islam <Admin@Saimons-MacBook-Air.local>
2020-08-11 21:48:44 +0600

    including node_modules

---
+++ build/precache-manifest.a5619b458f1564c8b896d8b9ab7ea118.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b48590838c92612cc53049ef1aee8375",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21ccbbd8fc2d3c989222",
+    "url": "/static/css/main.3d9cfc3f.chunk.css"
+  },
+  {
+    "revision": "5975bf7516a6a17f1386",
+    "url": "/static/js/2.d49404a8.chunk.js"
+  },
+  {
+    "revision": "21ccbbd8fc2d3c989222",
+    "url": "/static/js/main.dfb12216.chunk.js"
+  },
+  {
+    "revision": "70236f58620bce11adae",
+    "url": "/static/js/runtime-main.e50cdb56.js"
+  }
+]);
